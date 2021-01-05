ca578e7687da87a5564164df60112b0a2e35e1f6
Ayushman Gupta <ayushman1998.1998@gmail.com>
2021-01-05 14:46:53 +0530

    added fetch to job details

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,10 +1,8 @@
 <template>
-  <div>
-    <pre>
-      <code>
-        {{ id }} {{somethingElse}}
-        </code>
-      </pre>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <h3>{{ job.id }}</h3>
+    <p>{{ job.details }}</p>
   </div>
 </template>
 
@@ -12,6 +10,25 @@
 export default {
   name: "JobDetails",
   props: ["id", "somethingElse"],
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    this.fetchJob();
+  },
+  methods: {
+    async fetchJob() {
+      try {
+        const rawData = await fetch(`http://localhost:3001/jobs/${this.id}`);
+        const dataJson = await rawData.json();
+        this.job = dataJson;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
 };
 </script>
 
