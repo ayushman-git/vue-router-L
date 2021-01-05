d8b404b82bd78efa0cca99ed3fda45ea96c9f86a
Ayushman Gupta <ayushman1998.1998@gmail.com>
2021-01-05 13:03:42 +0530

    added jobs route, views and components structure

---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="(job, index) in jobs" :key="index">
+    <h2>{{ job.title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Jobs",
+  data() {
+    return {
+      jobs: [
+        { title: "Ninja UX Designer", id: 1, details: "lorem" },
+        { title: "Ninja Web Developer", id: 2, details: "lorem" },
+        { title: "Ninja Vue Developer", id: 3, details: "lorem" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
